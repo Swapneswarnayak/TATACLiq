d516dc9053a6c9090d23cc8a1671f2ace8e55428
Himangshu Mahato <you@example.com>
2022-11-28 16:30:49 +0530

    new branch created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Footer from "./Components/Footer/Footer";
+import {RiFacebookBoxFill} from 'react-icons/fa';
+
+function App() {
+
+  return (
+    <div className="App">
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import footer from "./footer.css"
+// import {RiFacebookBoxFill} from 'react-icons
+import {FaFacebookF } from "react-icons/fa";
+
+function Footer(props) {
+    return (
+        <div>
+            <div className='firstpart'>
+            <div><h4> Tata Trust</h4></div>
+            <div><h4> QUiQ PiQ</h4></div>
+            <div><h4> Authentic Brands</h4></div>
+            <div><h4> Easy Returns</h4></div>
+            </div>
+            <div className='secondPart'>
+             <div className='leftPart'>
+             <ul> <h4>Tata MarketPlace</h4>
+               <li style={{paddingTop:"10px"}}>About Us</li>
+                <li> Careers</li>
+                <li> Sell With Us</li>
+                <li> Terms of Use</li>
+                <li> Privacy Policy</li>
+                <li>Affiliates</li>
+                <li>Sitemap</li>
+             </ul>
+             <ul> <h4>Customer Service</h4>
+              <li style={{paddingTop:"10px"}}> Shopping</li>
+              <li>Offers & Promotions</li>
+              <li>   Payments</li>
+              <li>  Cancellation</li>
+              <li>Returns & Refunds</li>
+              <li>CliQ And PiQ</li>
+              <li> Return To Store</li>
+              <li> Electronics Return Policy</li>
+              <li> Contact Us</li>
+              <li>  Reviews Guidelines</li>
+              <li> Furniture Return Policy</li>
+              <li> Replacement Policy
+              </li>
+             </ul>
+             <ul> <h4>My Tata CLiQ</h4>
+              <li style={{paddingTop:"10px"}}>
+             My Account</li>
+             <li> My Orders</li>
+             <li> My Shopping Bag</li>
+             <li> My Wishlist       
+             </li>
+             </ul>
+             </div>
+             <div className='rightPart'> 
+              <ul>
+              <h4> Tata CLiQ Offerings</h4>
+               <li style={{paddingTop:"10px"}}>Watches for Men   |  Campus Shoes  |  Sandals for Men  | Sneakers for Men</li>
+               <li>Reebok Shoes  |  Cotton Kurtis | Woodland Shoes  | Jumpsuits| Allen Solly</li>
+               <li>Sparx Shoes  |  Gold Rings  |  Formal Shoes for Men  |  Sports Shoes for Men |</li> 
+               <li>Wallets for Men |  Ladies Watches   |  Trolley Bags  |  Handbags for Women  |</li>
+               <li>Sling Bags for Women  |   Casual Shoes for Men  |  Boots for Men  |  Digital Watches</li>
+               <li> Sonata Watches |  Sneakers for Women  |  Running Shoes  |  Puma Shoes  |</li>
+               <li> Boots for Women  |  Skechers |Malabargold  |  Fabindia  |  Utsa  |  Vark  |  Gia  |  LOV  |Sitemap  Boots for Women  | </li>
+              </ul>
+             </div>
+    
+            </div> 
+            <div className='icon'>
+            <div className='icon-d'>
+            <div className='d'>
+            Download App
+            <div> <img src="https://github.com/SUMAN-24/Tata-Cliq/blob/main/icons/android.png?raw=true" alt="" /></div>
+            <div> <img src="https://github.com/SUMAN-24/Tata-Cliq/blob/main/icons/apple.png?raw=trues" alt="" /></div>
+            </div>
+           
+           
+            </div>
+            <div className='logo'>
+               <div className='logoc'>
+               <div>
+               <img src="https://github.com/SUMAN-24/Tata-Cliq/blob/main/icons/facebook.png?raw=true" alt="" />
+               </div>
+               <div>      <img src="https://github.com/SUMAN-24/Tata-Cliq/blob/main/icons/twitter.png?raw=true" alt="" /></div>
+
+               <div> <img src="https://github.com/SUMAN-24/Tata-Cliq/blob/main/icons/insta.png?raw=true" alt="" /></div>
+               <div>    <img src="https://github.com/SUMAN-24/Tata-Cliq/blob/main/icons/youtube.png?raw=true" alt="" /></div>
+              
+               <div> <img src="https://github.com/SUMAN-24/Tata-Cliq/blob/main/icons/linkedin.png?raw=true" alt="" /></div>
+
+
+
+               </div>
+
+
+            </div>
+            <div className='lasticon'> <div> © 2022 Tata CLiQ | All rights reserved </div></div>
+            </div>
+            <div className='lastPart'>
+               <div>
+                <h4>TATA CLiQ: Shop Online with India's most trusted destination</h4>
+               <p>Genuine products from all the top brands get delivered right to your doorstep. Our sleek, immersive design allows you to easily navigate between categories 
+               and brand stores so that you can find a wide selection of <a href=""> womenswear, menswear, kidswear, footwear, watches, accessories, footwear, watches</a>  and  <a href="">accessories </a> online.
+                You can also check our great offers and get the best prices on
+                various products across lifestyle, fashion, and more.</p>
+               </div>
+               <div>
+               <h4>Online Shopping: Fast & convenient with the click of a button</h4>
+               <p>              
+               The upside of online shopping at TATA CLiQ online store, is that you'll save on time and most importantly money with TataCliq offers. It's as simple as comparing products and prices online before making the right buy. What's more, you also have the option to pay for your favourite brands and products using our easy EMI options. Of course, you can buy
+               and try - in the convenience of your home. Returns are easy too: We'll pick up your returns for free or you can also drop off the goods at the nearest brand store.
+               </p>
+               
+               
+               </div>
+               <div>
+               <h4> 
+               Tata CLiQ Shopping App: just a few clicks on Android & iOS  </h4>
+               <p>Download the Android app from the <a href=""> Play Store </a>or the iOS app from <a href=""> Apple App Store </a> and get set to enjoy a range of benefits. Apart from the best deals, amazing offers and the latest styles online, the app also gives you the flexibility to shop at your convenience. Use the easy share options to share your shopping with your friends and family to ensure you're buying something perfect. With constant updates and a host of new features being introduced constantly, enjoy a shopping experience that you'll love.
+               </p>
+               </div>
+               <div>
+               <h4> 
+               TATA CLiQ: The most genuine place for Fashion and Lifestyle</h4>
+               <p>With an exclusive Brand Store for <a href="">  Westside Online </a> we have most of your trendy shopping needs taken care of. Make TATA CLiQ your online shopping destination and get the best deals on your favourite brands, with 100% genuine products. Be it jewellery or makeup, you can count on TATA CLiQ for receiving only the most authentic products.</p>
+               </div>
+
+
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
