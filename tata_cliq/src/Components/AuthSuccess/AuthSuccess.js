514e7cba6977c88fcb4afb804c7ddae2cb1d2871
Anjali280 <mondalanjali0707@gmail.com>
2022-12-04 19:19:42 +0530

    LOG OUT

--- tata_cliq/src/Components/AuthSuccess/AuthSuccess.js
+++ tata_cliq/src/Components/AuthSuccess/AuthSuccess.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import Nav from "../Nav/Nav";
 
 const AuthSuccess = ({ validate }) => {
-  return (
-    <div style={{ marginTop: "50px" }}>
-      {validate()}
-      <img
-        src="https://www.tatacliq.com/src/mobile-number-login/images/sucess_login.svg"
-        alt="success"
-      />
-      <div style={{ marginTop: "30px" }}>
-        <h2>You're Successfully</h2>
-        <h2>Logged In</h2>
-        <h2>Start CLiQuing</h2>
+  const timeOut = useRef({});
+  useEffect(() => {
+    setTimeout(() => {
+      timeOut.current = 1;
+    }, 1000);
+  }, []);
+
+  if (timeOut.current === 1) {
+    return <Nav />;
+  } else {
+    return (
+      <div style={{ marginTop: "50px" }}>
+        {validate()}
+        <img
+          src="https://www.tatacliq.com/src/mobile-number-login/images/sucess_login.svg"
+          alt="success"
+        />
+        <div style={{ marginTop: "30px" }}>
+          <h2>You're Successfully</h2>
+          <h2>Logged In</h2>
+          <h2>Start CLiQuing</h2>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default AuthSuccess;
