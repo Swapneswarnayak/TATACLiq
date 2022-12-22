d3e902986013d5a6cfc6e6790a412387b0c45708
Swapneswarnayak <1705835@kiit.ac.in>
2022-12-22 23:28:09 +0530

    l

--- src/Cart_and_Payment/Component/Addcoupon/Modal.js
+++ src/Cart_and_Payment/Component/Addcoupon/Modal.js
@@ -14,7 +14,7 @@ import myCartContext from "../../CartContext/Cartcontext";
 import React, { useContext } from "react";
 import { Highlight } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
-//modal componentl
+//modal componentlj
 
 export default function BasicUsage() {
   const { isOpen, onOpen, onClose } = useDisclosure();
